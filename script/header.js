0a2f2a9c1f52938ce4b32a65f1a2fbf51177b223
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-09-30 15:37:59 +0200

    modification du index.html avec ajout du header, images et script

---
+++ script/header.js
@@ -0,0 +1,6 @@
+const menuBurger = document.querySelector('.menu-burger');
+const menu = document.querySelector('.menu');
+
+menuBurger.addEventListener("click", function(){
+    menu.classList.toggle('active');
+})
